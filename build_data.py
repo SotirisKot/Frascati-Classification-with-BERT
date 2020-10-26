638fb8f5fe78b08c1500a85136772e8e644c0489
Sotiris <sotiriskot9@gmail.com>
2020-10-26 19:59:03 +0200

    Update build_data.py

--- build_data.py
+++ build_data.py
@@ -1,7 +1,7 @@
 import pickle
 import json
 import os
-from handle_data import parse_mag_dump, split_train_dev_test
+from utils import parse_mag_dump, split_train_dev_test
 
 parse_mag_dump()
 split_train_dev_test()
