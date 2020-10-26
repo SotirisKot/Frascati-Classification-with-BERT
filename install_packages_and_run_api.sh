66967e0ae4d7c59e511a36c1e4ba84553573501d
Sotiris <sotiriskot9@gmail.com>
2020-10-26 14:49:17 +0200

    Add files via upload

--- install_packages_and_run_api.sh
+++ install_packages_and_run_api.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PWD=$(pwd)
 echo "$PWD"
-python3.6 -m virtualenv $PWD/frascati_classifier_env
+python3.6 -m virtualenv -p python3.6 $PWD/frascati_classifier_env
 set -e
 source $PWD/frascati_classifier_env/bin/activate
 python3.6 -m pip3 install -r requirements.txt
