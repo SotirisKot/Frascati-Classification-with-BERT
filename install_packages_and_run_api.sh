826928065bd311842592330e110e26fddd735c83
Sotiris <sotiriskot9@gmail.com>
2020-10-26 14:55:55 +0200

    Add files via upload

--- install_packages_and_run_api.sh
+++ install_packages_and_run_api.sh
@@ -5,4 +5,4 @@ python3.6 -m virtualenv -p python3.6 $PWD/frascati_classifier_env
 set -e
 source $PWD/frascati_classifier_env/bin/activate
 python3.6 -m pip install -r requirements.txt
-python3.6 frascati_classifier_api.py
+deactivate
