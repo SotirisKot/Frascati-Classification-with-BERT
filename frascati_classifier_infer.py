b47dd31eda372c22939f7bc9ce2f6c1a11f4041b
dvpappas <dvpappas89@gmail.com>
2020-11-06 16:58:45 +0200

    altered the how_many part

--- frascati_classifier_infer.py
+++ frascati_classifier_infer.py
@@ -347,7 +347,7 @@ def infer_for_top_k(publication_dict, k):
         l1_classifier_res   = [i.split('/')[1] for i in predictions_of_second_level]
         l2_classifier_res   = [(all_subclasses[i], str(subcats_logits.squeeze()[i].cpu().item())) for i in order]
         l1_max_voted        = max(Counter(l1_classifier_res))
-        l2_max_voted        = [t for t in l2_classifier_res if t[0]==l1_max_voted]
+        l2_max_voted        = [t for t in l2_classifier_res if l1_max_voted in t[0]]
         l2_max_voted        = max(l2_max_voted, key = lambda x:x[1])
 
         # prediction_of_second_level = all_subclasses[torch.argmax(subcats_logits).cpu().numpy()]
